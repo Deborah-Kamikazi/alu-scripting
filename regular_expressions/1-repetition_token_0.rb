f4950802a2769b7437caf7d40068119cbb4e1558
Deborah-Kamikazi <deborahkamikazi9@gmail.com>
2025-09-16 03:06:37 -0700

    Implement 1-repetition_token_0.rb to find repeated sequences

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -6,7 +6,7 @@ input = ARGV[0]
 
 # Regex pattern with repetition token
 # Example: matches 'hbt', 'hbtn', 'hbtnt', etc. depending on instructions
-pattern = /hbt+n/
+pattern = /hbtn/
 
 # Print all matches without spaces
 matches = input.scan(pattern)
