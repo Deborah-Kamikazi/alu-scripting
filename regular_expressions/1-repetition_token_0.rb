ed06f85a1e36d78a6ea978773f84fb4ed6e4f9c8
Deborah-Kamikazi <deborahkamikazi9@gmail.com>
2025-09-16 03:01:09 -0700

    Implement 1-repetition_token_0.rb to find repeated sequences

---
+++ regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# This script searches for a pattern with repetition in the given argument
+
+# Get the first argument
+input = ARGV[0]
+
+# Regex pattern with repetition token
+# Example: matches 'hbt', 'hbtn', 'hbtnt', etc. depending on instructions
+pattern = /hbt+n/
+
+# Print all matches without spaces
+matches = input.scan(pattern)
+puts matches.join
