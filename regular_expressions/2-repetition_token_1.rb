9df22e3db66ff458110cdd6741956e657e92d0d6
deborah <deborahkamikazi9@gmail>
2025-09-17 11:36:25 +0200

    Implement 2-repetition_token_1.rb to find repeated sequences

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-# This script prints matches of a specific pattern with repetition
-input = ARGV[0]
-# Regex pattern
-# Adjust the pattern based on your assignment instructions
-# Example: 'h' + optional 'b' + 't' repeated 2 or more times + 'n'
-pattern = /hb?t{2,}n/
-
-matches = input.scan(pattern)
-puts matches.join
+puts ARGV[0].scan(/hb?tn/).join
